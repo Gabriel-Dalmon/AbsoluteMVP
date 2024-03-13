0dc5abd6252f6327107c34c7175bd88833805ec5
deadly <vgauter@gaming.tech>
2024-03-13 20:44:10 +0100

    shitty spheric collisions get more opt

--- SleepyEngine/src/components/ColliderHandmade.cpp
+++ SleepyEngine/src/components/ColliderHandmade.cpp
@@ -5,13 +5,11 @@ bool ColliderHandMade::collideTest(Entity* self, Entity* target)
 	XMVECTOR selfPos = XMLoadFloat3(&self->GetComponent<Transform*>()->m_positionVect);
 	XMVECTOR targetPos = XMLoadFloat3(&target->GetComponent<Transform*>()->m_positionVect);
 
-	float deltaX = XMVectorGetX(selfPos) * XMVectorGetX(selfPos) - XMVectorGetX(targetPos) * XMVectorGetX(targetPos);
-	float deltaY = XMVectorGetY(selfPos) * XMVectorGetY(selfPos) - XMVectorGetY(targetPos) * XMVectorGetY(targetPos);
-	float deltaZ = XMVectorGetZ(selfPos) * XMVectorGetZ(selfPos) - XMVectorGetZ(targetPos) * XMVectorGetZ(targetPos);
+	XMVECTOR deltaPos = selfPos * selfPos - targetPos * targetPos;
 
 	float deltaRadius = self->GetComponent<ColliderHandMade*>()->m_radius + target->GetComponent<ColliderHandMade*>()->m_radius;
 
-	if (deltaX < deltaRadius && deltaY < deltaRadius && deltaZ < deltaRadius) {
+	if (XMVectorGetX(deltaPos) + XMVectorGetY(deltaPos) + XMVectorGetZ(deltaPos) < deltaRadius * deltaRadius) {
 		return true;
 	}
 	
