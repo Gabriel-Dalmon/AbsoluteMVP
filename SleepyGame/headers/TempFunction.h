c3228788a4a9f12bac0b7f4c248aea557e1d8832
kindhee <rmichel@gaming.tech>
2024-03-13 11:03:54 +0100

    temp function

---
+++ SleepyGame/headers/TempFunction.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class TempFunction : public SleepyEngine
+{
+public:
+
+	TempFunction();
+	~TempFunction() {};
+
+	void Init() override;
+	void Blank() override;
+
+private:
+
+};
+
---
+++ SleepyGame/src/TempFunction.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+
+#include "TempFunction.h"
+#include "../../SleepyEngine/headers/SleepyEngine.h"
+
+
+TempFunction::TempFunction()
+{
+}
+
+void TempFunction::Init()
+{
+}
+
+void TempFunction::Blank()
+{
+}
