3e5e25f60a5dcd37ca5f023a71b2fb021591a3b6
kindhee <rmichel@gaming.tech>
2024-03-13 12:47:16 +0100

    fill

--- SleepyGame/src/GameFactory.cpp
+++ SleepyGame/src/GameFactory.cpp
@@ -12,6 +12,9 @@ void GameFactory::FillPlayer(Entity* pEntity)
 	transform->Identity();
 	pEntity->AddComponent<Transform*>(transform);
 
+	Velocity* velocity = Component::CreateComponent<Velocity>();
+	velocity->SetVelocity(0.0f, 0.0f, 0.0f);
+
 	ColliderSphereDescriptor CSDesc;
 	CSDesc.center = { 0, 0, 0 };
 	CSDesc.radius = 1;
@@ -27,10 +30,15 @@ void GameFactory::FillPlayer(Entity* pEntity)
 	std::cerr << typeid(*meshRef).name();
 
 	ShaderReferenceDescriptor ShaderRefDesc;
-	//ShaderRefDesc.PSO = m_PSOTexture;
+
+	// for color only object :
 	ShaderRefDesc.PSO = m_PSOColor;
-	//ShaderRefDesc.rootSignature = m_pRootSignatureTexture;
 	ShaderRefDesc.rootSignature = m_pRootSignatureColor;
+
+	// for textured object :
+	//ShaderRefDesc.PSO = m_PSOTexture;
+	//ShaderRefDesc.rootSignature = m_pRootSignatureTexture;
+
 	ShaderRefDesc.id = 1;
 	ShaderReference* shaderReference = Component::CreateComponent<ShaderReference>();
 	shaderReference->Init(&ShaderRefDesc);
@@ -44,11 +52,79 @@ void GameFactory::FillPlayer(Entity* pEntity)
 
 void GameFactory::FillEnemy(Entity* pEntity)
 {
+	Transform* transform = Component::CreateComponent<Transform>();
+	transform->Identity();
+	pEntity->AddComponent<Transform*>(transform);
 
+	Velocity* velocity = Component::CreateComponent<Velocity>();
+	velocity->SetVelocity(0.0f, 0.0f, 0.0f);
+
+	ColliderSphereDescriptor CSDesc;
+	CSDesc.center = { 0, 0, 0 };
+	CSDesc.radius = 1;
+	ColliderSphere* collider = Component::CreateComponent<ColliderSphere>();
+	collider->Init(&CSDesc);
+	pEntity->AddComponent<ColliderSphere*>(collider);
+
+	MeshReferenceDescriptor MeshRefDesc;
+	MeshRefDesc.meshRef = m_pRessourceAllocator->getMesh("cube"); // change to an enemy mesh 
+	MeshRenderer* meshRef = Component::CreateComponent<MeshRenderer>();
+	meshRef->Init(&MeshRefDesc);
+	pEntity->AddComponent<MeshRenderer*>(meshRef);
+	std::cerr << typeid(*meshRef).name();
+
+	ShaderReferenceDescriptor ShaderRefDesc;
+
+	// for color only object :
+	ShaderRefDesc.PSO = m_PSOColor;
+	ShaderRefDesc.rootSignature = m_pRootSignatureColor;
+
+	// for textured object :
+	//ShaderRefDesc.PSO = m_PSOTexture;
+	//ShaderRefDesc.rootSignature = m_pRootSignatureTexture;
+
+	ShaderRefDesc.id = 1; // chnage to id of enemy texture
+	ShaderReference* shaderReference = Component::CreateComponent<ShaderReference>();
+	shaderReference->Init(&ShaderRefDesc);
+	pEntity->AddComponent<ShaderReference*>(shaderReference);
 };
 
 
 void GameFactory::FillBullet(Entity* pEntity)
 {
+	Transform* transform = Component::CreateComponent<Transform>();
+	transform->Identity();
+	pEntity->AddComponent<Transform*>(transform);
 
+	Velocity* velocity = Component::CreateComponent<Velocity>();
+	velocity->SetVelocity(0.0f, 0.0f, 0.0f);
+
+	ColliderSphereDescriptor CSDesc;
+	CSDesc.center = { 0, 0, 0 };
+	CSDesc.radius = 1;
+	ColliderSphere* collider = Component::CreateComponent<ColliderSphere>();
+	collider->Init(&CSDesc);
+	pEntity->AddComponent<ColliderSphere*>(collider);
+
+	MeshReferenceDescriptor MeshRefDesc;
+	MeshRefDesc.meshRef = m_pRessourceAllocator->getMesh("cube"); // change to a bullet mesh
+	MeshRenderer* meshRef = Component::CreateComponent<MeshRenderer>();
+	meshRef->Init(&MeshRefDesc);
+	pEntity->AddComponent<MeshRenderer*>(meshRef);
+	std::cerr << typeid(*meshRef).name();
+
+	ShaderReferenceDescriptor ShaderRefDesc;
+
+	// for color only object :
+	ShaderRefDesc.PSO = m_PSOColor;
+	ShaderRefDesc.rootSignature = m_pRootSignatureColor;
+
+	// for textured object :
+	//ShaderRefDesc.PSO = m_PSOTexture;
+	//ShaderRefDesc.rootSignature = m_pRootSignatureTexture;
+
+	ShaderRefDesc.id = 1; // change to id of bullet texture
+	ShaderReference* shaderReference = Component::CreateComponent<ShaderReference>();
+	shaderReference->Init(&ShaderRefDesc);
+	pEntity->AddComponent<ShaderReference*>(shaderReference);
 };
--- SleepyGame/src/TempFunction.cpp
+++ SleepyGame/src/TempFunction.cpp
@@ -16,11 +16,17 @@ void TempFunction::BlankUpdate()
 
 void TempFunction::BlankPreLoop()
 {
+    OutputDebugStringA("Attempting to load entities\n");
+
     Entity* player = Entity::CreateEmptyEntity();
     ((GameFactory*)m_pFactory)->FillPlayer(player);
     m_entities.push_back(player);
-    MeshRenderer* temp;
-    temp = player->GetComponent<MeshRenderer*>();
-    mBoxGeo = temp->GetMesh();
-    OutputDebugStringA("entity loaded\n");
+    OutputDebugStringA("entity player loaded\n");
+
+    Entity* enemy = Entity::CreateEmptyEntity();
+    ((GameFactory*)m_pFactory)->FillEnemy(enemy);
+    m_entities.push_back(enemy);
+    OutputDebugStringA("entity enemy loaded\n");
 }
+
+
